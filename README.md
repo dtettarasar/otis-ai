986e9f8769d6fe65a440bda0f1746f80bc96e01b
Dylan Tettarasar <dtettarasar@gmail.com>
2023-06-18 12:50:37 +0200

    added element to setup configuration

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # otis-ai
 Otis AI: Your Digital SEO Scribe! 
+
+## Add your configuration and API : 
+create a json file inside the configuration_files folder, and use the following format to include your org datas : 
+
+{
+    "organization": "your org ID",
+    "apiKey": "your API key"
+}
--- app.js
+++ app.js
@@ -1,2 +1,44 @@
-console.log('hello world');
-console.log('test');
+import { Configuration, OpenAIApi } from "openai";
+import * as fs from 'fs';
+
+
+let rawdata = fs.readFileSync('configuration_files/config01.json');
+console.log(rawdata);
+let configData = JSON.parse(rawdata);
+console.log(configData);
+
+
+/*
+const openai = new OpenAIApi(configuration);
+*/
+
+/*
+const chatCompletion = await openai.createChatCompletion({
+    model:"gpt-3.5-turbo",
+    messages: [
+        {role: "user", content: "bonjour!"},
+    ]
+});
+*/
+
+// exemple de requête : écris moi un article de 600 mots sur la data fabric, au format markdown
+
+//console.log(chatCompletion.data.choices[0].message);
+
+
+/*
+try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Hello world",
+    });
+    console.log(completion.data.choices[0].text);
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+  }
+*/
