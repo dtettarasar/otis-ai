c3d98c0e0356db8d9e7c00386606279767149dca
Dylan Tettarasar <dtettarasar@gmail.com>
2023-06-19 03:24:26 -0400

    update in app.js to make it use the config json file

--- app.js
+++ app.js
@@ -1,44 +1,29 @@
 import { Configuration, OpenAIApi } from "openai";
 import * as fs from 'fs';
 
-
-let rawdata = fs.readFileSync('configuration_files/config01.json');
-console.log(rawdata);
+let rawdata = fs.readFileSync('configuration_files/config.json');
 let configData = JSON.parse(rawdata);
 console.log(configData);
 
+const configuration = new Configuration({
+    "organization": configData.organization,
+    "apiKey": configData.apiKey
+});
 
-/*
 const openai = new OpenAIApi(configuration);
-*/
 
-/*
 const chatCompletion = await openai.createChatCompletion({
     model:"gpt-3.5-turbo",
     messages: [
-        {role: "user", content: "bonjour!"},
+        {role: "user", content: "hello world"},
     ]
 });
-*/
+
 
 // exemple de requête : écris moi un article de 600 mots sur la data fabric, au format markdown
 
-//console.log(chatCompletion.data.choices[0].message);
+console.log(chatCompletion.data.choices[0].message);
 
-
-/*
-try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Hello world",
-    });
-    console.log(completion.data.choices[0].text);
-  } catch (error) {
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-    } else {
-      console.log(error.message);
-    }
-  }
-*/
+// Build 2 functions
+// Generate article from a topics (string)
+// Generate article from an array of keywords
