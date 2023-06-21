2fedc814967c87aca91c665d828a0877b1d07358
Dylan Tettarasar <dtettarasar@gmail.com>
2023-06-21 12:05:59 -0400

    update class

--- app.js
+++ app.js
@@ -9,26 +9,42 @@ const configuration = new Configuration(configData);
 
 const openai = new OpenAIApi(configuration);
 
-const language = {
-    fr: 'french',
-    en: 'english',
-    es: 'spanish'
-}
-
-const textRequest = `write an article optimized for search engine. to define the topics of the article and the lexical field, use the following keywords: datacenter, ecology, environment. 
-it should be written in markdown format. the language of the article should be ${language.en}.
-the article should contain subtitles for each section. at the end of the article, add a section add a section in which you mention the sources used to create the article`;
-
-console.log(textRequest);
-
 class ArticleReq {
-    constructor(keywords) {
+
+    constructor(keywords, chosenLanguage) {
+
         this.keywords = [keywords];
+
+        this.language = {
+            fr: 'french',
+            en: 'english',
+            es: 'spanish'
+        };
+
+        this.textRequest = `write an article optimized for search engine. to define the topics of the article and the lexical field, use the following keywords: ${this.keywords}. 
+        it should be written in markdown format. the language of the article should be ${this.language[chosenLanguage]}.
+        the article should contain subtitles for each section. at the end of the article, add a section in which you mention the sources used to create the article`;
+
     }
+
+    getTextRequest() {
+        return this.textRequest;
+    }
+
+    /*
+    generateArticle = await openai.createChatCompletion({
+        model:"gpt-3.5-turbo",
+        messages: [
+            {role: "user", content: this.textRequest},
+        ]
+    });
+    */
+
 }
 
-const myArticleTest = new ArticleReq(["datacenter", "ecology"]);
-console.log(myArticleTest);
+const articleObj = new ArticleReq(["datacenter", "ecology", "environmental issues"], 'fr');
+//console.log(articleObj);
+console.log(articleObj.getTextRequest());
 
 /*
 const chatCompletion = await openai.createChatCompletion({
