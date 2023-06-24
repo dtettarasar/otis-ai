fc24c996efd0574539bf69e614de355f6f88b0b5
Dylan Tettarasar <dtettarasar@gmail.com>
2023-06-24 11:54:40 -0400

    created initApiConfigMethod

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 import * as fs from 'fs';
 
-let rawdata = fs.readFileSync('configuration_files/config.json');
-let configData = JSON.parse(rawdata);
-console.log(configData);
-
-const configuration = new Configuration(configData);
-
-const openai = new OpenAIApi(configuration);
-
 class Article {
 
     constructor(keywords, chosenLanguage) {
@@ -24,7 +16,31 @@ class Article {
         this.textRequest = `write an article optimized for search engine. to define the topics of the article and the lexical field, use the following keywords: ${this.keywords}. 
         it should be written in markdown format. the language of the article should be ${this.language[chosenLanguage]}.
         the article should contain subtitles for each section. at the end of the article, add a section in which you mention the sources used to create the article.
-        Make sure the sources you're adding are pages that exists`;
+        Make sure the sources you're using are pages that are available on the web, and not fake or dead links`;
+
+    }
+
+    initApiConfig() {
+
+        let configData = null;
+
+        try {
+
+            let rawdata = fs.readFileSync('configuration_files/config.json');
+            configData = JSON.parse(rawdata);
+        
+        } catch (error) {
+        
+            console.log(error);
+            return false;
+        
+        }
+
+        console.log(configData);
+        const configuration = new Configuration(configData);
+        const openai = new OpenAIApi(configuration);
+
+        return openai;
 
     }
 
@@ -34,16 +50,27 @@ class Article {
 
     async generateArticle() {
 
-        const chatCompletion = await openai.createChatCompletion({
-            model:"gpt-3.5-turbo",
-            messages: [
-                {role: "user", content: this.getTextRequest()},
-            ]
-        });
+        const openai = this.initApiConfig();
 
-        const openAiResponse = chatCompletion.data.choices[0].message
+        if (openai) {
 
-        console.log(openAiResponse);
+            const chatCompletion = await openai.createChatCompletion({
+                model:"gpt-3.5-turbo",
+                messages: [
+                    {role: "user", content: this.getTextRequest()},
+                ]
+            });
+    
+            const openAiResponse = chatCompletion.data.choices[0].message
+    
+            console.log(openAiResponse);
+
+        } else {
+
+            return false;
+
+        }
+
     }
 
 }
