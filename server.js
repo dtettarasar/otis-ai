c4d23947e4ac30b5b9135b6ab4390ec6a1a59677
Dylan Tettarasar <dtettarasar@gmail.com>
2023-07-27 07:49:00 -0400

    build initial elements to run the server

--- server.js
+++ server.js
@@ -3,3 +3,33 @@ const cors = require('cors');
 const cookieSession = require("cookie-session");
 const app = express();
 
+const corsOptions = {
+    origin: "http://localhost:8081"
+}
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+
+    cookieSession({
+      name: "otis-ai-session",
+      keys: ["COOKIE_SECRET"], // should use as secret environment variable
+      httpOnly: true
+    })
+    
+);
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Otis AI!" });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
