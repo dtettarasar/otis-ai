b5c90e2466a1ca5e6693062c7eab180326bae0de
Dylan Tettarasar <dtettarasar@gmail.com>
2023-08-10 03:57:20 -0400

    renamed str_hasher

---
+++ custom_modules/str_hasher.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcryptjs");
+const password = "mypass123"
+const saltRounds = 10
+
+const strHasher = {
+
+    getHash: (strToHash) => {
+
+        bcrypt.genSalt(saltRounds, (saltErr, salt) => {
+
+            if (saltErr) {
+                throw saltErr;
+            } else {
+                bcrypt.hash(strToHash, salt, (hashErr, hash) => {
+                    if (hashErr) {
+                        throw hashErr;
+                    } else {
+                        console.log(hash);
+                    }
+                })
+            }
+    
+        })
+
+    }
+
+}
+
+const hashed = strHasher.getHash(password);
+console.log(hashed);
+
+//console.log();
