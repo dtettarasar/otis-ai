e26188e9d48dcb9a41cdd70227b7af5fc3bda314
Dylan Tettarasar <dtettarasar@gmail.com>
2023-08-11 12:00:07 -0400

    update in user model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+/*
 const strHasher = require("../../custom_modules/str_hasher.js");
 console.log(strHasher);
-
-const password = "mypass123"
-
+const password = "mypass123";
 strHasher.config.getHash(password);
+*/
 
-console.log('user model');
+//console.log('user model');
 
 const UserSch = new mongoose.Schema({
     username: {type: String, required: true}, 
@@ -19,6 +20,44 @@ const UserSch = new mongoose.Schema({
         }
     ]
 });
+
+// pre middleware function, to hash users' passwords
+UserSch.pre("save", (next) => {
+
+    const user = this;
+
+    if (this.isModified("password") || this.isNew) {
+
+        bcrypt.genSalt(10, (saltErr, salt) => {
+
+            if (saltErr) {
+
+                return next(saltErr);
+
+            } else {
+
+                bcrypt.hash(user.password, salt, (hashErr, hash) => {
+
+                    if (hashErr) {
+
+                        return next(hashErr);
+
+                    } else {
+
+                        user.password = hash;
+                        next();
+
+                    }
+
+                })
+
+            }
+
+        })
+
+    }
+
+})
 
 const User = mongoose.model(
     "User", UserSch
