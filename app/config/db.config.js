77e0bdf5a039c7562aaf22d5b6d0ca9e731c90e5
Dylan Tettarasar <dtettarasar@gmail.com>
2023-08-21 17:14:26 +0200

    update database class

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 const env = require('dotenv').config();
 const mongoose = require('mongoose');
 const roleModel = require('../models/role.model');
-const userModel = require('../models/user.model');
+const UserModel = require('../models/user.model');
 
 class DataBase {
 
@@ -26,23 +26,43 @@ class DataBase {
 
     async createUser(req, res) {
 
-        const userObj = new userModel({
+        const userObj = new UserModel({
             username: req.body.username,
             email: req.body.email,
             password: req.body.psw
-        });  
+        });
+
+        const usernameInDB = await this.findUserByName(userObj.username);
+        console.log("check if username already exist");
+        console.log(usernameInDB);
         
         try {
 
             const user = await userObj.save();
             console.log(user);
             res.json(user);
+            
         } catch (err) {
     
             console.log(err);
             res.json({Error: err});
     
         }
+
+    }
+
+    async findUserByName(username) {
+
+        //console.log("find username method");
+        //console.log("username to check: " + username);
+
+        const query = UserModel.find({username: username});
+        query.select('username');
+        const userFound = await query.exec();
+        //console.log(typeof userFound);
+
+        return userFound;
+
 
     }
 
