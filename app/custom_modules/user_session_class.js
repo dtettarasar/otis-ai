5b65d2f524e22597ab4c4ad0e40359d69974a14d
Dylan Tettarasar <dtettarasar@gmail.com>
2023-08-22 11:07:59 -0400

    update user session class

--- app/custom_modules/user_session_class.js
+++ app/custom_modules/user_session_class.js
@@ -1,3 +1,10 @@
+const env = require('dotenv').config();
+const mongoose = require('mongoose');
+const UserModel = require('../models/user.model');
+
+const dataBaseClass = require('../config/db.config');
+const dataBase = new dataBaseClass();
+
 class UserSession {
 
     constructor () {
@@ -5,7 +12,16 @@ class UserSession {
     }
 
     async createSession (req, res) {
-        res.json({test: "test login"});
+
+        const userObj = {
+            username: req.body.username,
+            password: req.body.psw
+        };
+
+        const usernameInDB = await dataBase.findUserByName(userObj.username);
+        console.log(usernameInDB);
+
+        res.json(userObj);
     }
 
 } 
