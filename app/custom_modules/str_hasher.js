c8fffafc217e059a396e2dbea6a9fb9306a65327
Dylan Tettarasar <dtettarasar@gmail.com>
2023-08-23 22:50:15 +0200

    update in str_hasher

--- app/custom_modules/str_hasher.js
+++ app/custom_modules/str_hasher.js
@@ -1,46 +1,8 @@
 const bcrypt = require("bcryptjs");
 const password = "mypass123";
-const passwordHash = "$2a$10$8xPneLBVQajfh4HaDCsmfeRd11vv9t8xCBt1ICcql6CYIkkKu/INy";
-const passwordWrongHash = "$2a$10$jPOp0sr7w.eWVS5yLjR3KO6cMYnA7zJHnAMBapOKMW4b4eoCdbyvS";
-const saltRounds = 10;
+const passwordHash = "$2a$10$bUR/.yETRC3kdT1xOUyBNuJCpwtYwsPLLHrsqfWcTQtRzJutL17Qq";
 
 const strHasher = {
-
-    getHash: (strToHash) => {
-
-        bcrypt.genSalt(saltRounds, (saltErr, salt) => {
-
-            if (saltErr) {
-                throw saltErr;
-            } else {
-                bcrypt.hash(strToHash, salt, (hashErr, hash) => {
-                    if (hashErr) {
-                        throw hashErr;
-                    } else {
-                        console.log(hash);
-                    }
-                })
-            }
-    
-        })
-
-    },
-
-    checkHash: (strToCheck, hashToCheck) => {
-
-        bcrypt.compare(strToCheck, hashToCheck, (err, isMatch) => {
-
-            if (err) {
-                throw err;
-              } else if (!isMatch) {
-                console.log("Password doesn't match!");
-              } else {
-                console.log("Password matches!");
-              }
-
-        })
-
-    },
 
     genHashedStr: async (strToHash) => {
 
@@ -57,10 +19,36 @@ const strHasher = {
 
         }
 
+    },
+
+    checkHash: async (strToCheck, hashToCheck) => {
+
+        try {
+
+            const testStr = await bcrypt.compare(strToCheck, hashToCheck);
+
+            if (testStr) {
+
+                console.log("Password matches!");
+                return true;
+
+            } else {
+
+                console.log("Password doesn't match!");
+                return false;
+
+            }
+
+        } catch (err) {
+
+            console.log(err); 
+            return false;
+
+        }
+
     }
 
 }
-
 
 const testHash = async () => {
 
@@ -69,11 +57,14 @@ const testHash = async () => {
 
 }
 
-testHash();
+const testHashChecker = async () => {
 
-//strHasher.checkHash(password, passwordHash);
+    const result = await strHasher.checkHash(password, passwordHash);
+    console.log(result);
 
-//console.log(hashed);
+}
 
+//testHash();
+//testHashChecker();
 
 exports.config = strHasher;
