2e326cd34d23e242b0a2c4fec49927d8152d8de7
Dylan Tettarasar <dtettarasar@gmail.com>
2023-10-26 18:05:26 +0000

    create webhook route

--- routes/stripe_payment_checkout_routes.js
+++ routes/stripe_payment_checkout_routes.js
@@ -9,10 +9,10 @@ const express = require('express');
 const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
+// get the secret here: https://dashboard.stripe.com/webhooks
+const stripeEndpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
+
 const PORT = process.env.PORT || 8080;
-
-//const YOUR_DOMAIN = `http://localhost:${PORT}`;
-
 
 router.post('/create-checkout-session', async(req, res) => {
 
@@ -44,6 +44,63 @@ router.post('/create-checkout-session', async(req, res) => {
     });
 
     res.redirect(303, session.url);
+})
+
+router.post('/webhook', express.raw({type: 'application/json'}), (req, res) => {   
+
+    // Only verify the event if you have an endpoint secret defined.
+    // Otherwise use the basic event deserialized with JSON.parse
+
+    let event = req.body;
+
+    if (stripeEndpointSecret) {
+
+        // Get the signature sent by Stripe
+        const signature = request.headers['stripe-signature'];
+
+        try {
+
+            event = stripe.webhooks.constructEvent(
+                request.body,
+                signature,
+                endpointSecret
+            );
+
+
+        } catch (err) {
+
+            console.log(`Webhook signature verification failed.`, err.message);
+            return response.sendStatus(400);
+
+        }
+
+    }
+
+    // Handle event
+    switch (event.type) {
+
+        case 'payment_intent.succeeded':
+            const paymentIntent = event.data.object;
+            console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+            // Then define and call a method to handle the successful payment intent.
+            // handlePaymentIntentSucceeded(paymentIntent);
+            break;
+        
+        case 'payment_method.attached':
+            const paymentMethod = event.data.object;
+            // Then define and call a method to handle the successful attachment of a PaymentMethod.
+            // handlePaymentMethodAttached(paymentMethod);
+            break;
+
+        default:
+            // Unexpected event type
+            console.log(`Unhandled event type ${event.type}.`);
+
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.sendStatus(200);
+
 })
 
 router.get('/success', (req, res) => {
