79c056d3977435cecec417c2419b402963087840
Dylan Tettarasar <dtettarasar@gmail.com>
2023-11-27 15:42:46 +0000

    built my article route

--- routes/article_routes.js
+++ routes/article_routes.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send('article route');
+const userTokenClass = require('../app/custom_modules/user_token_class');
+const userToken = new userTokenClass();
+
+const dataBaseClass = require('../app/config/db.config');
+const dataBase = new dataBaseClass();
+dataBase.initDB();
+
+router.get("/", userToken.authToken, async (req, res) => {
+
+    const tokenData = {
+        Success: true,
+        accessToken: req.signedCookies.token,
+        refreshToken: req.signedCookies.refreshToken,
+        user: req.user
+    }
+
+    const userInfo = {
+        userId: req.user['_id'],
+        username: await dataBase.getUserName(req.user['_id']),
+        credit: await dataBase.getUserCrd(req.user['_id'])
+    };
+
+    console.log("access to article route");
+    console.log(userInfo);
+
+    res.render('article/my-article');
+
 });
 
 module.exports = router;
