bc79ef21e3594a55a258cb19d73a000f77dd0750
Dylan Tettarasar <dtettarasar@gmail.com>
2024-03-22 18:00:37 +0000

    update router file

--- vue-client/src/router/index.js
+++ vue-client/src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import AllUserArticlesView from '@/views/AllUserArticlesView.vue'
+import ArticleView from '@/views/ArticleView.vue'
+import CreateArticleView from '@/views/CreateArticleView.vue'
+import EditArticleView from '@/views/EditArticleView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import UserAccountView from '@/views/UserAccountView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +21,41 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/all-user-article',
+      name: 'all user article',
+      component: AllUserArticlesView
+    },
+    {
+      path: '/view-article',
+      name: 'view article',
+      component: ArticleView
+    },
+    {
+      path: '/create-article',
+      name: 'create article',
+      component: CreateArticleView
+    },
+    {
+      path: '/edit-article',
+      name: 'edit article',
+      component: EditArticleView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/user-account',
+      name: 'user account',
+      component: UserAccountView
     }
   ]
 })
