4f3a5c0fcb0dc7ae5015a6134cd8892dc87887dd
Dylan Tettarasar <dtettarasar@gmail.com>
2024-03-24 18:00:29 +0000

    build the login form component

--- vue-client/src/components/forms/LoginForm.vue
+++ vue-client/src/components/forms/LoginForm.vue
@@ -1,7 +1,29 @@
 <template>
 
     <div>
-        login form component
+        
+        <form action="#" method="post">
+
+            <p>Please use the form below to login to your account.</p>
+
+            <div class="mb-3">
+
+                <label class="form-label" for="username"><b>Username</b></label>
+                <input class="form-control" type="text" placeholder="Enter Username" name="username" id="username" required>
+
+            </div>
+
+            <div class="mb-3">
+
+                <label class="form-label" for="psw"><b>Password</b></label>
+                <input class="form-control" type="password" placeholder="Enter Password" name="psw" id="psw" required>
+                
+            </div>
+
+            <button type="submit" class="btn btn-primary">Login</button>
+
+        </form>
+
     </div>
 
 </template>
