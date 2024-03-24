6979cdf859e4fab1ad852bba54a760baf7c3980e
Dylan Tettarasar <dtettarasar@gmail.com>
2024-03-24 11:00:33 +0000

    build navbar component

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -2,22 +2,13 @@
 import { RouterLink, RouterView } from 'vue-router';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from '@/components/NavBar.vue'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/all-user-article">All User Article</RouterLink>
-        <RouterLink to="/view-article">View article</RouterLink>
-        <RouterLink to="/create-article">Create Article</RouterLink>
-        <RouterLink to="/edit-article">Edit article</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/register">Register</RouterLink>
-        <RouterLink to="/user-account">User Account</RouterLink>
-      </nav>
+      <NavBar></NavBar>
     </div>
   </header>
 
---
+++ vue-client/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><RouterLink to="/">Otis</RouterLink></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="#"><RouterLink to="/">Home</RouterLink></a>
+                    <a class="nav-link" href="#"><RouterLink to="/about">About</RouterLink></a>
+                    <a class="nav-link" href="#"><RouterLink to="/all-user-article">All User Article</RouterLink></a>
+                    <a class="nav-link" href="#"><RouterLink to="/view-article">View article</RouterLink></a>
+                    <a class="nav-link" href="#"><RouterLink to="/create-article">Create Article</RouterLink></a>
+                    <a class="nav-link" href="#"><RouterLink to="/edit-article">Edit article</RouterLink></a>
+                    <a class="nav-link" href="#"><RouterLink to="/login">Login</RouterLink></a>
+                    <a class="nav-link" href="#"><RouterLink to="/register">Register</RouterLink></a>
+                    <a class="nav-link" href="#"><RouterLink to="/user-account">User Account</RouterLink></a>
+                </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        name: 'NavBar',
+    }
+
+</script>
+
+<style>
+
+</style>
