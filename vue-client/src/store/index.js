542ec7e644a9c59c7602adc673aa8211d874ceee
Dylan Tettarasar <dtettarasar@gmail.com>
2024-03-25 08:35:41 +0000

    add comment in vuex store file

---
+++ vue-client/src/store/index.js
@@ -0,0 +1,4 @@
+/* créer une variable bool userLogged. Pour identifier si un utilisateur est connecté. 
+Permettra de conditionner l'affichage de certains composants + gérer les requêtes pour alimenter le store. 
+userLogged sera à utiliser pour vérifier qu'un utilisateur est bien connecté, pour faire les requêtes au backend (récupérer username, crédit, id des articles, etc...)
+*/
