42b3ec930ccb06b967a02ab9a9ee49500880255a
Dylan Tettarasar <dtettarasar@gmail.com>
2024-03-26 18:53:55 +0000

    build element for connexion with backend api

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -7,11 +7,22 @@ import App from './App.vue';
 import router from './router';
 //import store from './store'
 
-const app = createApp(App)
+import backendConfig from './backend.config';
+
+const app = createApp(App);
+
+let backendUrl;
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+   backendUrl = backendConfig.development;
+} else {
+   backendUrl = backendConfig.production;
+}
+
+console.log(backendUrl);
+
 
 app.use(router);
 //app.use(store)
 
 app.mount('#app');
-
-console.log("hello world");
