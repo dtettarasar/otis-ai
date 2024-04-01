64d0934e05ee6baf29392014fb448f33e9ab42ab
Dylan Tettarasar <dtettarasar@gmail.com>
2024-04-01 15:40:35 +0000

    vue login process: find user by username

--- backend/routes/front_api_routes.js
+++ backend/routes/front_api_routes.js
@@ -2,15 +2,28 @@
 
 const express = require('express');
 const router = express.Router();
+const dataBaseObj = require('../app/custom_modules/database_obj');
 
 router.post('/user-login', async (req, res) => {
-    console.log('test post request /vc-login');
-    console.log(req);
-    res.json({
-        test: 'test the login form vue component',
+    console.log('test post request');
+    //console.log(req);
+
+    const userObj = {
+        existInDB: null,
         username: req.body.username,
         password: req.body.password
-    });
+    }
+
+    const usernameInDB = await dataBaseObj.findUserByName(userObj.username);
+    console.log(usernameInDB);
+
+    if (usernameInDB.length === 1 && usernameInDB[0].username === userObj.username) {
+        userObj.existInDB = true;
+    } else {
+        userObj.existInDB = false;
+    }
+
+    res.json(userObj);
 });
 
 module.exports = router;
