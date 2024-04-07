ad8a8bc2662f8e6772f33d7597dc8998894417f3
Dylan Tettarasar <dtettarasar@gmail.com>
2024-04-07 17:11:36 +0200

    create new custom module for login process

---
+++ vue-client/src/custom_modules/createAxiosInstance.js
@@ -0,0 +1,17 @@
+// src/custom_modules/api.js
+
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+// Fonction utilitaire pour créer une instance Axios avec les headers d'authentification
+export const createAxiosInstance = () => {
+  const accessToken = Cookies.get('accessToken');
+  return axios.create({
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  });
+};
+
+// Instance Axios avec les headers d'authentification
+export const axiosInstance = createAxiosInstance();
