d6f8c83fe687d1e9277b375c499067db010f2704
Dylan Tettarasar <dtettarasar@gmail.com>
2024-04-11 08:41:35 +0200

    update the menu component

--- vue-client/src/components/NavBar.vue
+++ vue-client/src/components/NavBar.vue
@@ -8,17 +8,20 @@
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <router-link class="nav-link" to="/">Home</router-link>
-                    <router-link class="nav-link" to="/about">About</router-link>
-                    <router-link class="nav-link" to="/all-user-article">All User Article</router-link>
-                    <router-link class="nav-link" to="/view-article">View article</router-link>
-                    <router-link class="nav-link" to="/create-article">Create Article</router-link>
-                    <router-link class="nav-link" to="/edit-article">Edit article</router-link>
-                    <router-link class="nav-link" to="/login">Login</router-link>
-                    <router-link class="nav-link" to="/register">Register</router-link>
-                    <router-link class="nav-link" to="/user-account">User Account</router-link>
-                </div>
+                    <div class="navbar-nav">
+                        
+                        <router-link class="nav-link" to="/">Home</router-link>
+                        <router-link class="nav-link" to="/about">About</router-link>
+                        <router-link v-if="this.userLoggedIn" class="nav-link" to="/all-user-article">All User Article</router-link>
+                        <router-link v-if="this.userLoggedIn" class="nav-link" to="/view-article">View article</router-link>
+                        <router-link v-if="this.userLoggedIn" class="nav-link" to="/create-article">Create Article</router-link>
+                        <router-link v-if="this.userLoggedIn" class="nav-link" to="/edit-article">Edit article</router-link>
+                        <router-link v-if="this.userLoggedIn" class="nav-link" to="/user-account">User Account</router-link>
+                        <router-link v-if="!this.userLoggedIn" class="nav-link" to="/login">Login</router-link>
+                        <router-link v-if="!this.userLoggedIn" class="nav-link" to="/register">Register</router-link>
+                        <a v-on:click="initLoggout" href="#" v-if="this.userLoggedIn" class="nav-link" >Loggout</a>
+
+                    </div>
                 </div>
             </div>
         </nav>
@@ -28,8 +31,26 @@
 
 <script>
 
+    import { mapState } from 'vuex';
+
     export default {
+        
         name: 'NavBar',
+
+        computed: {
+
+            ...mapState(['userLoggedIn'])
+
+        },
+
+        methods: {
+
+            initLoggout () {
+                console.log('init the user loggout method');
+            }
+
+        }
+
     }
 
 </script>
