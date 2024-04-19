cea1fb130624b0c4540f2ba47d89d87990101036
Dylan Tettarasar <dtettarasar@gmail.com>
2024-04-19 11:25:41 +0000

    str encrypter test: fixed the first test

--- backend/app/custom_modules/str_encrypter.test.js
+++ backend/app/custom_modules/str_encrypter.test.js
@@ -1,13 +1,21 @@
-import { expectTypeOf } from 'vitest';
+import { expect, test } from 'vitest';
 const strEncrypter = require('./str_encrypter');
 
 const testStr = "this is a test string";
 
+let testEncryption = {};
+
 test('test the encrypter: make sure it returns an object', async () => {
 
-    const testEncryption = await strEncrypter.method.encryptString(testStr);
-    console.log(testEncryption);
+    testEncryption = await strEncrypter.method.encryptString(testStr);
+    //console.log(testEncryption);
 
-    expectTypeOf(testEncryption).toBeObject();
+    expect(testEncryption).toBeTypeOf('object');
+
+});
+
+test('test the object returned by the encrypter', async () => {
+
+    console.log(testEncryption);
 
 });
