42d5b142bb9fe73a0bfc94c351610c71632a19f2
Dylan Tettarasar <dtettarasar@gmail.com>
2024-05-18 18:13:14 +0200

    user token obj: update test script

--- backend/app/custom_modules/user_token_obj.test.js
+++ backend/app/custom_modules/user_token_obj.test.js
@@ -9,7 +9,8 @@ let dbConnection;
 testUserObj.generateUser('user with correct parameters','DummyTestlady', 'dummy.testlady', '@otis-ai-test.eu', 'Test001!');
 testUserObj.generateUser("user with correct parameters, for this one we'll test login with wrong password", "Pilou", "king.pilou", '@otis-ai-test.eu', 'Test001!');
 testUserObj.generateUser("user that won't be created in the database", "Natty", "queen.natty", '@otis-ai-test.eu', 'Test001!');
-testUserObj.generateUser("user with correct parameters, created to test failed token creation", "CafeTheGuineaPig", "cafe.guinea.pig", '@otis-ai-test.eu', 'Test001!');
+testUserObj.generateUser("user with correct parameters, created to test failed token creation and auth token (no jwt will be provided)", "CafeTheGuineaPig", "cafe.guinea.pig", '@otis-ai-test.eu', 'Test001!');
+testUserObj.generateUser("user with correct parameters, created to test failed auth token (with invalid key)", "CracotteTheGuineaPig", "cracotte.guinea.pig", '@otis-ai-test.eu', 'Test001!');
 
 beforeAll(async () => {
 
@@ -17,6 +18,7 @@ beforeAll(async () => {
     await testUserObj.testUserCreation(0);
     await testUserObj.testUserCreation(1);
     await testUserObj.testUserCreation(3);
+    await testUserObj.testUserCreation(4);
 
 });
   
@@ -31,14 +33,16 @@ test('test checkUserLogin method', async() => {
     testUserObj.userCont[1].authResult = await userTokenObj.checkUserLogin(testUserObj.userCont[1].username, 'didou&dede');
     testUserObj.userCont[2].authResult = await userTokenObj.checkUserLogin(testUserObj.userCont[2].username, testUserObj.userCont[2].password);
     testUserObj.userCont[3].authResult = await userTokenObj.checkUserLogin(testUserObj.userCont[3].username, testUserObj.userCont[3].password);
+    testUserObj.userCont[4].authResult = await userTokenObj.checkUserLogin(testUserObj.userCont[4].username, testUserObj.userCont[4].password);
 
     // Check the authSuccess
     await expect(testUserObj.userCont[0].authResult).toHaveProperty('authSuccess', true);
     await expect(testUserObj.userCont[1].authResult).toHaveProperty('authSuccess', false);
     await expect(testUserObj.userCont[2].authResult).toHaveProperty('authSuccess', false);
     await expect(testUserObj.userCont[3].authResult).toHaveProperty('authSuccess', true);
+    await expect(testUserObj.userCont[4].authResult).toHaveProperty('authSuccess', true);
 
-    console.log(testUserObj.userCont);
+    // console.log(testUserObj.userCont);
     // console.log(testUserObj.userCont[0].authResult);
 
     // Check that users contain the userIdEncryption object
@@ -54,12 +58,18 @@ test('test checkUserLogin method', async() => {
     await expect(testUserObj.userCont[3].authResult).toHaveProperty('userIdEncryption');
     await expect(testUserObj.userCont[3].authResult.userIdEncryption).toBeInstanceOf(Object);
 
+    await expect(testUserObj.userCont[4].authResult).toHaveProperty('userIdEncryption');
+    await expect(testUserObj.userCont[4].authResult.userIdEncryption).toBeInstanceOf(Object);
+
     // Check that the userIdEncryption object has the iv & encryptedStr properties (for the the user successfully logged in)
     await expect(testUserObj.userCont[0].authResult.userIdEncryption).toHaveProperty('iv');
     await expect(testUserObj.userCont[0].authResult.userIdEncryption).toHaveProperty('encryptedStr');
 
     await expect(testUserObj.userCont[3].authResult.userIdEncryption).toHaveProperty('iv');
     await expect(testUserObj.userCont[3].authResult.userIdEncryption).toHaveProperty('encryptedStr');
+
+    await expect(testUserObj.userCont[4].authResult.userIdEncryption).toHaveProperty('iv');
+    await expect(testUserObj.userCont[4].authResult.userIdEncryption).toHaveProperty('encryptedStr');
 
 
     // Regex to check encryptedStr & iv formats
@@ -72,35 +82,50 @@ test('test checkUserLogin method', async() => {
     await expect(testUserObj.userCont[3].authResult.userIdEncryption.iv).toMatch(ivRegex);
     await expect(testUserObj.userCont[3].authResult.userIdEncryption.encryptedStr).toMatch(encryptedStrRegex);
 
+    await expect(testUserObj.userCont[4].authResult.userIdEncryption.iv).toMatch(ivRegex);
+    await expect(testUserObj.userCont[4].authResult.userIdEncryption.encryptedStr).toMatch(encryptedStrRegex);
+
 });
 
 test('test create token method', async() => {
     
     testUserObj.userCont[0].tokenResult = await userTokenObj.createToken(testUserObj.userCont[0].authResult, process.env.ACCESS_TOKEN_SECRET, process.env.ACCESS_TOKEN_EXP);
-    console.log(testUserObj.userCont[0]);
+    // console.log(testUserObj.userCont[0]);
 
     testUserObj.userCont[3].tokenResult = await userTokenObj.createToken(testUserObj.userCont[3].authResult, false, process.env.ACCESS_TOKEN_EXP);
-    console.log(testUserObj.userCont[3]);
+    // console.log(testUserObj.userCont[3]);
+
+    testUserObj.userCont[4].tokenResult = await userTokenObj.createToken(testUserObj.userCont[4].authResult, process.env.ACCESS_TOKEN_SECRET, process.env.ACCESS_TOKEN_EXP);
 
     // check that userCont[0].tokenResult contains a proper token
     await expect(typeof testUserObj.userCont[0].tokenResult).toBe('string');
+
+    // check that userCont[4].tokenResult contains a proper token
+    await expect(typeof testUserObj.userCont[4].tokenResult).toBe('string');
 
     // check that userCont[3].tokenResult is a bool and false;
     await expect(typeof testUserObj.userCont[3].tokenResult).toBe('boolean');
     await expect(testUserObj.userCont[3].tokenResult).toBe(false);
     
+    // check that userCont[0] & userCont[4] contains a token with proper format
     const jwtRegex = /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/;
     await expect(testUserObj.userCont[0].tokenResult).toMatch(jwtRegex);
+    await expect(testUserObj.userCont[4].tokenResult).toMatch(jwtRegex);
+
+    // console.log(testUserObj.userCont);
 
 });
 
 test('test auth token method', async() => {
 
     testUserObj.userCont[0].authTokenResult = await userTokenObj.authToken(testUserObj.userCont[0].tokenResult, process.env.ACCESS_TOKEN_SECRET);
-    console.log(testUserObj.userCont[0]);
+    // console.log(testUserObj.userCont[0]);
 
     testUserObj.userCont[3].authTokenResult = await userTokenObj.authToken(testUserObj.userCont[3].tokenResult, process.env.ACCESS_TOKEN_SECRET);
-    console.log(testUserObj.userCont[3]);
+    // console.log(testUserObj.userCont[3]);
+
+    testUserObj.userCont[4].authTokenResult = await userTokenObj.authToken(testUserObj.userCont[4].tokenResult, 'fake_secret_key');
+    // console.log(testUserObj.userCont[4].authTokenResult);
 
     await expect(testUserObj.userCont[0].authTokenResult).toBeInstanceOf(Object);
     await expect(testUserObj.userCont[0].authTokenResult.token).toBe(testUserObj.userCont[0].tokenResult);
@@ -119,5 +144,15 @@ test('test auth token method', async() => {
     await expect(testUserObj.userCont[3].authTokenResult.result).toBeInstanceOf(Object);
     await expect(testUserObj.userCont[3].authTokenResult.result.name).toBe('JsonWebTokenError');
     await expect(testUserObj.userCont[3].authTokenResult.result.message).toBe('jwt must be provided');
+
+    // Check the error handling : invalid key signature
+    await expect(testUserObj.userCont[4].authTokenResult).toBeInstanceOf(Object);
+    await expect(testUserObj.userCont[4].authTokenResult.token).toBe(testUserObj.userCont[4].tokenResult);
+    await expect(testUserObj.userCont[4].authTokenResult.status).toBe(false);
+    await expect(testUserObj.userCont[4].authTokenResult.result).toBeInstanceOf(Object);
+    await expect(testUserObj.userCont[4].authTokenResult.result.name).toBe('JsonWebTokenError');
+    await expect(testUserObj.userCont[4].authTokenResult.result.message).toBe('invalid signature');
+
+    console.log(testUserObj.userCont);
 
 });
