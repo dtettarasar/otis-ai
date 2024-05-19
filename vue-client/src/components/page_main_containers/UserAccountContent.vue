7b40fcae21534b3f768f6f00231bc9bf1cf08908
Dylan Tettarasar <dtettarasar@gmail.com>
2024-05-19 18:46:32 +0200

    build the credit section for the user account page

--- vue-client/src/components/page_main_containers/UserAccountContent.vue
+++ vue-client/src/components/page_main_containers/UserAccountContent.vue
@@ -1,19 +1,22 @@
 <template>
 
     <UserAccountBanner></UserAccountBanner>
+    <CreditSection></CreditSection>
 
 </template>
 
 <script>
 
     import { mapState } from 'vuex';
-    import UserAccountBanner from '@/components/page_banners/UserAccountBanner.vue'
+    import UserAccountBanner from '@/components/page_banners/UserAccountBanner.vue';
+    import CreditSection from '@/components/user_account_components/CreditSection.vue';
 
     export default {
         name: 'UserAccountContent',
 
         components: {
-            UserAccountBanner
+            UserAccountBanner,
+            CreditSection
         },
 
         data() {
---
+++ vue-client/src/components/user_account_components/CreditSection.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <p>This is the credit section</p>
+
+    <p>Test: user has {{ credit }} credit(s)</p>
+
+</template>
+
+<script>
+
+    import { mapState } from 'vuex';
+
+    export default {
+
+        name: 'CreditSection',
+
+        data() {
+
+            return {
+
+            }
+
+        },
+
+        computed: {
+            
+            ...mapState(['credit'])
+
+        },
+
+        mounted() {
+            //console.log(this.username);
+        },
+
+        beforeCreate() {
+        },
+
+        beforeMount() {
+        },
+
+        methods: {
+
+        }
+
+    }
+
+</script>
