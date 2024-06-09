8b90c505f004e050dacd9420d98368f6bdeeb2f8
Dylan Tettarasar <dtettarasar@gmail.com>
2024-06-09 20:33:56 +0200

    Update createArticleTrigger.js

--- vue-client/src/custom_modules/createArticleTrigger.js
+++ vue-client/src/custom_modules/createArticleTrigger.js
@@ -2,6 +2,17 @@ import axios from 'axios';
 import backendConfig from './../backend.config';
 import Cookies from 'js-cookie';
 
+/*
+TODO: 
+
+Mettre à jour la fonction pour qu'elle prenne en paramètre les infos de l'article provenant du formulaire (titre, description, contenu)
+Passer ces paramètres dans la requête pour que le backend puisse les sauvegarder dans la BDD
+Une fois que la création a été faite et validé par le backend, ce dernier doit retourner l'id ecnrypté de l'article. 
+
+Puis la fonction ici doit rediriger l'user vers view-article/:id
+
+*/
+
 let backendUrl;
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
