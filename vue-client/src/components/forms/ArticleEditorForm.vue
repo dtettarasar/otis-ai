25428c5b325af8fed28a77e602511c5f418bbba3
Dylan Tettarasar <dtettarasar@gmail.com>
2024-06-12 20:36:29 +0000

    building the article editor component

--- vue-client/src/components/forms/ArticleEditorForm.vue
+++ vue-client/src/components/forms/ArticleEditorForm.vue
@@ -1,9 +1,37 @@
 <template>
+    
     <div>
       <p v-if="isEditMode && articleObj">Editing Article ID: {{ articleObj.id }}</p>
       <p v-else>Creating a New Article</p>
       <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
     </div>
+
+    <div>
+
+      <form @submit.prevent="saveArticle" method="post" >
+
+        <div class="bg-dark-subtle rounded mt-4 mb-4 p-5">
+
+          <div class="mb-3">
+
+            <label for="articleTitle" class="form-label">Title</label>
+            <input type="text" class="form-control" id="articleTitle" placeholder="write here your amazing title">
+
+          </div>
+
+          <div class="mb-3">
+            <label for="articleDescription" class="form-label">Description</label>
+            <textarea class="form-control" id="articleDescription" rows="3"></textarea>
+          </div>
+
+        </div>
+
+        <button type="submit" class="btn btn-primary">Save</button>
+
+      </form>
+
+    </div>
+
   </template>
   
   <script>
@@ -33,6 +61,16 @@
           isEditMode: false
 
         };
+
+      },
+
+      methods: {
+
+        async saveArticle() {
+
+          console.log('init save article method');
+
+        },
 
       },
 
