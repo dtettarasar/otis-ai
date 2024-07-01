45ac3e14bcbf87c107739c3a3822c99f987822ab
Dylan Tettarasar <dtettarasar@gmail.com>
2024-07-01 16:44:27 +0000

    build the article card component

---
+++ vue-client/src/components/article_components/ArticleCard.vue
@@ -0,0 +1,60 @@
+<template>
+    
+    <div class="m-2 p-5 bg-dark-subtle rounded" >
+
+        <h1>title</h1>
+
+        <p class="fs-5 date-text">creation date: date value </p>
+        <p class="fs-5 date-text">last modification date: value </p>
+
+        <div>
+
+            <p class="fs-5 mt-4">keywords:</p>
+
+            <div class="mb-2 d-flex justify-content-start flex-wrap">
+
+            <div class="badge m-1 p-1 bg-primary keyword-bdge d-flex flex-row">
+                <p class="fs-6 m-1 align-self-center">keyword</p>
+            </div>
+
+            <div class="badge m-1 p-1 bg-primary keyword-bdge d-flex flex-row">
+                <p class="fs-6 m-1 align-self-center">keyword</p>
+            </div>
+
+            </div>
+
+            <div class="d-flex flex-row flex-wrap">
+
+                <button class="btn btn-dark m-1 p-2"><i class="bi bi-eye-fill"></i> View</button>
+                <button class="btn btn-success m-1 p-2"><i class="bi bi-pen-fill"></i> Edit</button>
+                <button class="btn btn-danger m-1 p-2"><i class="bi bi-trash-fill"></i> Delete</button>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        name: 'ArticleCard',
+        props: {
+            articleId: {
+                type: String,
+                required: true
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+    .date-text {
+        margin-bottom: 0px;
+    }
+
+</style>
--- vue-client/src/components/page_main_containers/AllUserArticlesContent.vue
+++ vue-client/src/components/page_main_containers/AllUserArticlesContent.vue
@@ -5,22 +5,36 @@
         <h2>{{ username }} is connected to the All User Articles page</h2>
     </div>
 
-    <div>
-        article ID list: 
+    <div class="container">
 
-        {{ this.articleIds }}
+        <div class="row">
+
+            <div class="col-12 col-md-6 col-lg-4 mb-3" v-for="articleId in articleIds" :key="articleId">
+
+                <ArticleCard :articleId="articleId"></ArticleCard>
+
+            </div>
+
+        </div>
+
     </div>
-    
 
 </template>
 
 <script>
 
     import { mapState } from 'vuex';
+    import ArticleCard from '@/components/article_components/ArticleCard.vue';
     import { toRaw } from 'vue';
 
     export default {
         name: 'AllUserArticlesContent.vue',
+
+        components: {
+
+            ArticleCard
+
+        },
 
         data() {
 
@@ -56,3 +70,7 @@
     }
 
 </script>
+
+<style>
+
+</style>
